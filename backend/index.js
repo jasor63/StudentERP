ede0e306eaa95a7e4b7bcb4a7066fb0c430e148f
jasor63 <surhentharchess7@gmail.com>
2026-01-23 23:02:19 +0530

    secure secrets with dotenv and update gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const connectDB = require('./utils/db');
 const cors = require('cors')
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      " MONGO_URL"
+      process.env.MONGO_URL
     );
     console.log("MongoDB connected successfully");
   } catch (error) {
